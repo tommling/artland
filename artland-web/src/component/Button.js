4767227387fdbb73a615b7d33cb34c125de037cd
Thomas <tveale@mymakecents.com>
2017-10-21 21:15:03 -0400

    rm artland web

--- artland-web/src/component/Button.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Button extends Component {
-	render() {
-	// default button component
-	}
-}
-
-export default Button;
--- artland-web/src/module/module.js
+++
@@ -1,3 +0,0 @@
-const module = 'HelloWorld';
-
-export { module };
